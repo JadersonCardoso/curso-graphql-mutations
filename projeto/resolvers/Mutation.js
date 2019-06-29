54bcddb0db8eac6b73b9c3fe8d5a0a7de1ab360c
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-06-29 16:01:48 -0300

    usando input para inserir um novo usuário

--- projeto/resolvers/Mutation.js
+++ projeto/resolvers/Mutation.js
@@ -3,14 +3,14 @@ const { usuarios, proximoId } = require('../data/db')
 module.exports = {
     //{nome, email, idade}
 
-    novoUsuario(_, args) {
-        const emailExistente = usuarios.some(u => u.email === args.email)
+    novoUsuario(_, { dados }) {
+        const emailExistente = usuarios.some(u => u.email === dados.email)
         if( emailExistente) {
             throw new Error('Já existe e-mail cadastrado!')
         }
         const novo = {
             id: proximoId,
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO'
 
