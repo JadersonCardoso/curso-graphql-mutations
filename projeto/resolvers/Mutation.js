17eb9ded9e78eb317e1e6db2944c1d59c1486d03
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-06-30 16:07:26 -0300

    desafio alterar usuário com input aula 31

--- projeto/resolvers/Mutation.js
+++ projeto/resolvers/Mutation.js
@@ -36,13 +36,14 @@ module.exports = {
       return excluidos ? excluidos[0] : null
   },
 
-  alterarUsuario(_, args) {
-      const i = usuarios.findIndex(u => u.id === args.id)
+  alterarUsuario(_, {filtro, dados}) {
+      //const i = usuarios.findIndex(u => u.id === args.id)
+      const i = indiceUsuario(filtro)
       if( i < 0) return null
 
       const usuario = {
           ...usuarios[i],
-          ...args
+          ...dados
       }
 
       usuarios.splice(i, 1, usuario)
